49eac3691f45f8f7fa50941f9a2eddba4fb5d95f
hagenek <georghagen@gmail.com>
2021-02-01 20:18:34 +0100

    did hamming exercise

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,7 +1,4 @@
 (ns reverse-string)
 
 (defn reverse-string [s]
-  (loop [input-string s xs '()]
-    (if (empty? input-string)
-      (apply str xs)
-      (recur (rest input-string) (conj xs (first (seq input-string)))))))
+  (apply str (into '() s)))
