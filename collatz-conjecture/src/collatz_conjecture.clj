96ec74de8ca340dccbf24461bb7b3ccc66416095
hagenek <georghagen@gmail.com>
2021-02-09 19:10:08 +0100

    refactored Collatz-conjecture

--- collatz-conjecture/src/collatz_conjecture.clj
+++ collatz-conjecture/src/collatz_conjecture.clj
@@ -1,12 +1,12 @@
  (ns collatz-conjecture)
 
-(defn collatz [num]
-  (if (<= num 0)
-    (throw (AssertionError. "Wrong input"))
-    (loop [n num count 0]
-      (if (= n 1)
-        count
-        (if (even? n)
-          (recur (/ n 2) (inc count))
-          (recur (+ (* n 3) 1) (inc count)))))))
+(defn collatz-helper [n]
+  (if (= n 1)
+    n
+    (if (even? n)
+      (/ n 2)
+      (+ (* n 3) 1))))
 
+(defn collatz [n]
+  (assert (pos? n))
+  (count (take-while #(> % 1) (iterate collatz-helper n))))
